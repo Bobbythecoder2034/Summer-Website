838da512c1a36a651c1dcc336c51979bb5d0bbb0
Bobbythecoder2034 <kkoala110@gmail.com>
2025-08-08 17:47:57 -0700

    finished

--- scripts/eventImages.js
+++ scripts/eventImages.js
@@ -1,9 +1,18 @@
 //These images are only used for the events carousel
-const imageSRC = ["../images/arrow.png","../images/Adelie-penguin.webp","../images/images.jpg","../images/NationalGeographic_2772395.avif"]
-const imageAlt = ["an arrow","an Adelie penguin on rocks","A king penguin on ice","an orca pod"]
+const imageSRC = ["https://media1.giphy.com/media/v1.Y2lkPTZjMDliOTUyZmkybnZ1YTlhZDB5bDlmOHFqaDdkeGZwb2tnNjRmcXE0eWJyNGJkcSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/5t20H3tq99Y1DYFuEX/source.gif",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFd_8h3Ec-vgGV3iOe_DVKPtjhxh3MVEQjxA&s",
+    "https://i.makeagif.com/media/7-31-2018/oqMiu9.gif",
+    "https://i0.wp.com/media4.giphy.com/media/by7uJ8IGaeDmw/giphy.gif",
+  
+]
+const imageAlt = [
+    "PBS kids mascots building a house",
+    "ZONA camp 2025 banner",
+    "An orca show at Sea World",
+    "A pod of dolphins"]
 const descriptions = [
-    "arrow",
-    "adele",
-    "king",
-    "orca"
+    "I went on a mission trip with my church to build houses in Mexico. We were able to complete 7 homes in 3 days.",
+    "Me and my youth group went to ZONA camp in California for a week and had a great time.",
+    "Me and my siblings were visiting our grandparents in Texas and we went to see the orca show at Sea World.",
+    "We also saw a dolphin show at Sea World that was very entertaining and quite impressive."
 ]
--- scripts/galleryImages.js
+++ scripts/galleryImages.js
@@ -1,4 +1,17 @@
 //These images are only for the gallery carousel
-const imageSRC = ["../images/arrow.png","../images/Adelie-penguin.webp","../images/images.jpg"]
-const imageAlt = ["an arrow","an Adelie penguin on rocks","A king penguin on ice","an orca pod"]
-const descriptions = []
+const imageSRC = [
+    "https://static.vecteezy.com/system/resources/thumbnails/049/326/254/small_2x/oval-swimming-pool-with-clear-water-in-sunlight-cut-out-png.png",
+    "https://www.bluebell.com/wp-content/uploads/2021/02/blue-bell-ice-cream-single-serve-fudge-bar-unwrapped-WEB.png",
+    "https://mnd-assets.mynewsdesk.com/image/upload/c_fill,dpr_auto,f_auto,g_sm,q_auto:good,w_746/v85jyye7uyoqc851m514kf",
+    "https://gallery.yopriceville.com/downloadfullsize/send/8410"]
+const imageAlt = [
+    "a pool",
+    "a fudge bar",
+    "Magic the Gathering",
+    "a hot dog"]
+const descriptions = [
+    "The main spot to retreat from the triple degree weather",
+    "My favorite cold snack in the summer",
+    "Magic is a very summerish card game, wouldn't you agree?",
+    "I will die on the hill of hotdogs being the poster child of summer cuisine"
+]
